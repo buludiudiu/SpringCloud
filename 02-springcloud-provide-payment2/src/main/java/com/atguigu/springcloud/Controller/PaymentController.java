f6aa2208d392780a8e71712f6e4a8c65e4888e68
Administrator <1016056974@qq.com>
2021-04-09 16:05:09 +0800

    SpringCloud

--- 02-springcloud-provide-payment2/src/main/java/com/atguigu/springcloud/Controller/PaymentController.java
+++ 02-springcloud-provide-payment2/src/main/java/com/atguigu/springcloud/Controller/PaymentController.java
@@ -35,4 +35,8 @@ public class PaymentController {
         }
         return new CommonResult(444,"查询失败",null);
     }
+    @GetMapping("/payment/lb")
+    public String getPaymentLB(){
+        return serverPort;
+    }
 }
--- 03-springboot-consumer-order/src/main/java/com/atguigu/springboot/config/ApplicationContextConfig.java
+++ 03-springboot-consumer-order/src/main/java/com/atguigu/springboot/config/ApplicationContextConfig.java
@@ -9,8 +9,10 @@ import org.springframework.web.client.RestTemplate;
 public class ApplicationContextConfig {
 
     @Bean
-    @LoadBalanced
+    //@LoadBalanced
     public RestTemplate getRestTemplate(){
         return new RestTemplate();
     }
+
+
 }
